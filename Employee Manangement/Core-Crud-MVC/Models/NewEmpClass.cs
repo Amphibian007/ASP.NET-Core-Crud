33df473e8c365615c04fa8eafff5b2798ebd6055
Dipto Das <diptod111@gmail.com>
2020-07-30 14:21:36 +0600

    Initial work on employee management project

---
+++ Employee Manangement/Core-Crud-MVC/Models/NewEmpClass.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Core_Crud_MVC.Models
+{
+    public class NewEmpClass
+    {
+        [Key]
+
+        public int Empid { get; set; }
+
+        [Required(ErrorMessage ="Enter employee name: ")]
+
+        [Display(Name ="Employee Name")]
+
+
+        public string Empname { get; set; }
+        [Required(ErrorMessage = "Enter the email: ")]
+
+        [Display(Name = "Email")]
+
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Enter the age: ")]
+
+        [Display(Name = "Age")]
+
+        [Range(20,50)]
+        public int Age { get; set; }
+
+        [Required(ErrorMessage = "Enter the salary: ")]
+
+        [Display(Name = "Salary")]
+
+        public int Salary { get; set; }
+    }
+}
